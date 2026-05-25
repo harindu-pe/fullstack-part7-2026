1cd0f56dc0d4fede76ca4e78e35c71c41587ef1f
harindu-pe <harindu.pe@gmail.com>
2026-05-25 22:33:09 +0530

    exercise 7.4 completed

--- routed-anecdotes/src/hooks/index.js
+++ routed-anecdotes/src/hooks/index.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import anecdotesService from "../services/anecdotes";
 
 export const useField = (type) => {
   const [value, setValue] = useState("");
@@ -16,5 +17,33 @@ export const useField = (type) => {
     value,
     onChange,
     reset,
+  };
+};
+
+export const useAnecdotes = () => {
+  const [anecdotes, setAnecdotes] = useState([]);
+
+  const fetchAnecdotes = async () => {
+    try {
+      const data = await anecdotesService.getAll();
+      setAnecdotes(data);
+    } catch (error) {
+      console.error("Error fetching anecdotes:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAnecdotes();
+  }, []);
+
+  const addAnecdote = (anecdote) => {
+    setAnecdotes(
+      anecdotes.concat({ ...anecdote, id: Math.round(Math.random() * 10000) }),
+    );
+  };
+
+  return {
+    anecdotes,
+    addAnecdote,
   };
 };
