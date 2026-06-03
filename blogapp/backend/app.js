71f70db2cbe97336a96d5405cf24d30e49c575e0
harindu-pe <harindu.pe@gmail.com>
2026-06-03 18:30:13 +0530

    exercise 4.15 completed

--- blogapp/backend/app.js
+++ blogapp/backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 const config = require("./utils/config");
 
 const app = express();
@@ -10,5 +11,5 @@ mongoose.connect(config.MONGODB_URI);
 app.use(express.json());
 
 app.use("/api/blogs", blogsRouter);
-
+app.use("/api/users", usersRouter);
 module.exports = app;
