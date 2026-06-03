400332b36ad7d952b164f2114540a4002b06dc1a
harindu-pe <harindu.pe@gmail.com>
2026-06-03 18:49:57 +0530

    exercise 4.16 completed

--- blogapp/backend/controllers/users.js
+++ blogapp/backend/controllers/users.js
@@ -10,6 +10,12 @@ usersRouter.get("/", async (request, response) => {
 usersRouter.post("/", async (request, response) => {
   const { username, name, password } = request.body;
 
+  if (!username || username.length < 3) {
+    return response.status(400).json({
+      error: "Username is required and its minimum length is 3 characters",
+    });
+  }
+
   if (!password || password.length < 3) {
     return response.status(400).json({
       error: "Password is required and its minimum length is 3 characters",
