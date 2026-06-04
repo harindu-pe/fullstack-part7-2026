823a76f0f460da97332c23942f87dcbfdf3b9ea8
harindu-pe <harindu.pe@gmail.com>
2026-06-05 00:46:59 +0530

    exercise 5.1 completed

---
+++ blogapp/frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import Login from "./components/Login";
+
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  if (user === null) {
+    return <Login setUser={setUser} />;
+  }
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      <div>
+        <p>{user.name} logged in</p>
+      </div>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
---
+++ blogapp/frontend/src/components/Login.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import loginService from "../services/login";
+
+const Login = ({ setUser }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({ username, password });
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (e) {
+      console.log(e);
+      console.log("wrong credentials");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>
+            username
+            <input
+              type="text"
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            password
+            <input
+              type="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </label>
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
