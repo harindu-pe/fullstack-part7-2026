34e67b7b9567fa01b49467dc2ccb48ef6e18c9a5
harindu-pe <harindu.pe@gmail.com>
2026-06-05 15:14:57 +0530

    exercise 5.7 completed

--- blogapp/frontend/src/components/Blog.jsx
+++ blogapp/frontend/src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
+import Togglable from "./Togglable";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const [visible, setVisible] = useState(false);
+
+  const showWhenVisible = { display: visible ? "" : "none" };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <button onClick={() => setVisible(!visible)}>
+          {visible ? "hide" : "view"}
+        </button>
+      </div>
+      <div style={showWhenVisible}>
+        <div>{blog.url}</div>
+        <div>{blog.likes}</div>
+        <div>{blog.author}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
