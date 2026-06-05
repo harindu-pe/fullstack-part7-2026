3a5eaa8114a134b55a4dae60e6ac8c725672cd92
harindu-pe <harindu.pe@gmail.com>
2026-06-05 15:49:54 +0530

    exercise 5.9 and 5.10 completed

--- blogapp/frontend/src/App.jsx
+++ blogapp/frontend/src/App.jsx
@@ -13,6 +13,8 @@ const App = () => {
 
   const blogFormRef = useRef();
 
+  const sortedBlogs = [...blogs].sort((a, b) => b.likes - a.likes);
+
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
@@ -82,7 +84,7 @@ const App = () => {
         <BlogForm createBlog={addBlog} />
       </Togglable>
 
-      {blogs.map((blog) => (
+      {sortedBlogs.map((blog) => (
         <Blog key={blog.id} blog={blog} addLike={addLike} />
       ))}
     </div>
