dd519e9c9b4b443f7bbb01ddf75adc75b676bf8a
harindu-pe <harindu.pe@gmail.com>
2026-06-08 16:26:39 +0530

    exercise 5.17 completed

---
+++ blogapp/playwright/tests/blog_app.spec.js
@@ -0,0 +1,16 @@
+const { test, expect, beforeEach, describe } = require("@playwright/test");
+
+describe("Blog app", () => {
+  beforeEach(async ({ page }) => {
+    await page.goto("http://localhost:5173");
+  });
+
+  test("Login form is shown", async ({ page }) => {
+    // ...
+    const locator1 = page.getByRole("heading", { name: "Login" });
+    await expect(locator1).toBeVisible();
+
+    const locator2 = page.getByRole("button", { name: "login" });
+    await expect(locator2).toBeVisible();
+  });
+});
